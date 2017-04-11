b4d5c9ed83e846267b36e2c2f0648729af83efe7
gendoh30 <gendohs@naver.com>
2017-04-11 22:26:04 +0900

    init

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,13 @@
+@extends('app')
+
+@section('content')
+    <example></example>
+    <div class='content'>
+        <div class="title m-b-md">
+            DU Lab
+        </div>
+        <div class="links">
+            <a href='/intro'>intro</a>
+        </div>
+    </div>
+@endsection
